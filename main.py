351645705e013300d4e2c03fc329d9d5347d8c21
krishi_mahajan <krishimhjn@gmail.com>
2026-06-28 18:47:12 +0530

    new folder structure

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from fastapi import FastAPI
 app=FastAPI(title="Ai-Powered-support-ticket-system")
 
 
-app.get("/home")
+@app.get("/home")
 def root():
     return {"Hello":"Welcome to Ai-Powered-support-ticket-system"}
 
